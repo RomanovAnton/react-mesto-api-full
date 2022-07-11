f510c326dcc8501433f1cf09d5fe7cad821d9d94
Anton Romanov <rad250495@yandex.ru>
2022-07-11 21:46:50 +0700

    feat: CORS add headers/methods control

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,13 +2,26 @@ const allowedCors = [
   'https://mesto-rmv.nomorepartiesxyz.ru',
   'http://mesto-rmv.nomorepartiesxyz.ru',
   'localhost:3000',
+  'http://localhost:3000',
 ];
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
   next();
 };
